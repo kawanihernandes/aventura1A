8a35a43fd88c19ef8c57be5dde203b634183824a
kawani hernandes <kawani.hernandes@escola.pr.gov.br>
2025-09-16 08:04:59 -0300

    16/09

---
+++ script.js
@@ -0,0 +1,10 @@
+const avanca = document. querySelectorAll('.btn-proximo')
+
+avanca.forEach(button => {
+    button.addEventListenner('click', function (){
+        const atual = document.querySelector('.ativo');
+        const proximoPasso = 'passo' +
+    })
+}
+
+)
